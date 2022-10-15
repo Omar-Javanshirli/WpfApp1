cea22df45e22f0e6d690246af441a20b483fdfb2
stepguest <stepguest@STHQ012B-02>
2022-10-15 17:39:11 +0400

    Add project files.

---
+++ WpfApp1/Momento/Momento.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Momento
+{
+    public class Originator
+    {
+        private string _state;
+
+        public Originator(string state)
+        {
+            _state = state;
+            Console.WriteLine("Originator : My Initial state is : " + _state);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator : I am doing something important");
+            this._state = this.GenerateRandomString(30);
+        }
+        public string GenerateRandomString(int length = 30)
+        {
+            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = "";
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+                length--;
+            }
+            return result;
+        }
+        public IMemento Save()
+        {
+            return new TextMemento(this._state);
+        }
+
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is TextMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+            this._state = memento.GetState();
+        }
+    }
+    public interface IMemento
+    {
+        string GetName();
+        string GetState();
+        DateTime GetDate();
+    }
+    public class TextMemento : IMemento
+    {
+        private string _state;
+        private DateTime _date;
+        public TextMemento(string state)
+        {
+            this._state = state;
+            this._date = DateTime.Now;
+        }
+        public DateTime GetDate()
+        {
+            return _date;
+        }
+
+        public string GetName()
+        {
+            return $"{_date} / {_state.Substring(0, 9)}";
+        }
+
+        public string GetState()
+        {
+            return _state;
+        }
+    }
+    public class CareTaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+        private Originator _originator = null;
+
+        public CareTaker(Originator originator)
+        {
+            _originator = originator;
+        }
+
+        public void Undo()
+        {
+            if (_mementos.Count == 0)
+            {
+                return;
+            }
+
+            //var memento = _mementos.Last();
+            //this._mementos.Remove(memento);
+            var memento = _mementos[Index];
+            Index--;
+            Console.WriteLine("Care Taker :  Restoring state" + memento.GetName());
+
+            try
+            {
+                _originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+
+        }
+        public void Redo()
+        {
+            var memento = _mementos[Index];
+            Index++;
+            Console.WriteLine("Care Taker :  Restoring state" + memento.GetName());
+        }
+        public int Index { get; set; } = -1;
+        public void BackUp()
+        {
+            Console.WriteLine("\nCareTaker Saving Originator state . . . ");
+            this._mementos.Add(_originator.Save());
+            Index++;
+        }
+
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("CareTaker :  Here\'s the list of mementos");
+            foreach (var item in _mementos)
+            {
+                Console.WriteLine(item.GetName()); ;
+            }
+        }
+    }
+
+ 
+}
---
+++ WpfApp1/ViewModel/AppViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WpfApp1.Command;
+using WpfApp1.Momento;
+
+namespace WpfApp1.ViewModel
+{
+    public class AppViewModel:BaseViewModel
+    {
+        public RelayCommand ScreenShootCommand { get; set; }
+        public RelayCommand UndoCommand { get; set; }
+        public RelayCommand RendoCommand { get; set; }
+        private string imagePath;
+
+        public string ImagePath
+        {
+            get { return  imagePath; }
+            set {  imagePath = value; OnPropertyChanged(); }
+        }
+
+        public AppViewModel()
+        {
+            ScreenShot();
+        }
+
+        public void Undo()
+        {
+            UndoCommand = new RelayCommand((e) =>
+              {
+                  var momento = new CareTaker(imagePath);
+              });
+        }
+
+        public void ScreenShot()
+        {
+            ScreenShootCommand = new RelayCommand((e) =>
+            {
+                try
+                {
+                    //Creating a new Bitmap object
+                    Bitmap captureBitmap = new Bitmap(1024, 768, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                    //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+                    //Creating a Rectangle object which will
+                    //capture our Current Screen
+                    Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+                    //Creating a New Graphics Object
+                    Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+                    //Copying Image from The Screen
+                    captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+                    //Saving the Image File (I am here Saving it in My E drive).
+                    captureBitmap.Save(@"C:\Users\stepguest\Desktop\index\Capture.jpg", ImageFormat.Jpeg);
+                    //Displaying the Successfull Result
+
+                    ImagePath = @"C:\Users\stepguest\Desktop\index\Capture.jpg";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        
+        }
+
+    }
+}
